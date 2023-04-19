a18bf0a424b329245cd5cb19c1bd58d95c4b6ea7
prokusha <69151543+prokusha@users.noreply.github.com>
2023-04-19 06:03:01 +0300

    update tests

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,10 +1,8 @@
-// Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов
+
 void TestExcludeStopWordsFromAddedDocumentContent() {
     const int doc_id = 42;
     const string content = "cat in the city"s;
     const vector<int> ratings = {1, 2, 3};
-    // Сначала убеждаемся, что поиск слова, не входящего в список стоп-слов,
-    // находит нужный документ
     {
         SearchServer server;
         server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
@@ -13,9 +11,6 @@ void TestExcludeStopWordsFromAddedDocumentContent() {
         const Document& doc0 = found_docs[0];
         ASSERT_EQUAL(doc0.id, doc_id);
     }
-
-    // Затем убеждаемся, что поиск этого же слова, входящего в список стоп-слов,
-    // возвращает пустой результат
     {
         SearchServer server;
         server.SetStopWords("in the"s);
@@ -24,7 +19,7 @@ void TestExcludeStopWordsFromAddedDocumentContent() {
     }
 }
 
-void TestMinusPlusWords() {
+void TestSearchesWithTheMinusWord() {
     const int doc_id = 1;
     const string content = "what are you doing in my swamp";
     const vector<int> rating = {1, 3, 3};
@@ -57,12 +52,12 @@ void TestMatchDocuments() {
         const auto [words, status] = server.MatchDocument("make sense -not"s, 69);
         const auto [words_0, status_0] = server.MatchDocument("make sense -not"s, 70);
         ASSERT_HINT(words.empty(), "Minus word got on the list");
-        ASSERT_HINT(!words_0.empty(), "MatchDocument returned an empty list");
+        ASSERT_EQUAL_HINT(words_0.size(), 2, "MatchDocument returned an unexpected number of words");
         ASSERT_EQUAL_HINT(words_0, test, "The document does not match the request");
     }
 }
 
-void TestRelevanseAndRating() {
+void TestRelevanceSorting() {
     SearchServer server;
     server.SetStopWords("и в на"s);
     server.AddDocument(0, "белый кот и модный ошейник"s,        DocumentStatus::ACTUAL, {8, -3});
@@ -70,54 +65,93 @@ void TestRelevanseAndRating() {
     server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, {5, -12, 2, 1});
     server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
     {
-        const auto& respone = server.FindTopDocuments("пушистый ухоженный кот"s);
-        //Проверка порядка
-        ASSERT_EQUAL_HINT(respone[0].id, 1, "The order of the documents does not match the expectation");
-        ASSERT_EQUAL_HINT(respone[1].id, 0, "The order of the documents does not match the expectation");
-        ASSERT_EQUAL_HINT(respone[2].id, 2, "The order of the documents does not match the expectation");
-        //Проверка коректности значения релевантности
-        const double eps = 1e-6;
-        ASSERT_HINT(respone[0].relevance - 0.866434 < eps, "Relevance does not match the expectation");
-        ASSERT_HINT(respone[1].relevance - 0.173287 < eps, "Relevance does not match the expectation");
-        ASSERT_HINT(respone[2].relevance - 0.173287 < eps, "Relevance does not match the expectation");
-        //Проверка среднего рейтинга
-        ASSERT_EQUAL_HINT(respone[0].rating, 5, "Rating does not match the expectation");
-        ASSERT_EQUAL_HINT(respone[1].rating, 2, "Rating does not match the expectation");
-        ASSERT_EQUAL_HINT(respone[2].rating, -1, "Rating does not match the expectation");
+        const auto& response = server.FindTopDocuments("пушистый ухоженный кот"s);
+        ASSERT_EQUAL_HINT(response.size(), 3, "The number of results does not meet expectation");
+        ASSERT_EQUAL_HINT(response[0].id, 1, "The order of the documents does not match the expectation");
+        ASSERT_EQUAL_HINT(response[1].id, 0, "The order of the documents does not match the expectation");
+        ASSERT_EQUAL_HINT(response[2].id, 2, "The order of the documents does not match the expectation");
     }
 }
 
-void TestPeredicantAndStatus() {
+void TestCorrectnessOfRelevanceCalculation() {
+    SearchServer server;
+    server.SetStopWords("и в на"s);
+    server.AddDocument(0, "белый кот и модный ошейник"s,        DocumentStatus::ACTUAL, {8, -3});
+    server.AddDocument(1, "пушистый кот пушистый хвост"s,       DocumentStatus::ACTUAL, {7, 2, 7});
+    server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, {5, -12, 2, 1});
+    server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
+    {
+        const auto& response = server.FindTopDocuments("пушистый ухоженный кот"s);
+        const double eps = 1e-6;
+        ASSERT_EQUAL_HINT(response.size(), 3, "The number of results does not meet expectation");
+        ASSERT_HINT(response[0].relevance - 0.866434 < eps, "Relevance does not match the expectation");
+        ASSERT_HINT(response[1].relevance - 0.173287 < eps, "Relevance does not match the expectation");
+        ASSERT_HINT(response[2].relevance - 0.173287 < eps, "Relevance does not match the expectation");
+
+    }
+}
+
+void TestCorrectnessOfRatingCalculation() {
+    SearchServer server;
+    server.SetStopWords("и в на"s);
+    server.AddDocument(0, "белый кот и модный ошейник"s,        DocumentStatus::ACTUAL, {8, -3});
+    server.AddDocument(1, "пушистый кот пушистый хвост"s,       DocumentStatus::ACTUAL, {7, 2, 7});
+    server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, {5, -12, 2, 1});
+    server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
+    {
+        const auto& response = server.FindTopDocuments("пушистый ухоженный кот"s);
+        ASSERT_EQUAL_HINT(response.size(), 3, "The number of results does not meet expectation");
+        ASSERT_EQUAL_HINT(response[0].rating, (7 + 7 + 2)/3, "Rating does not match the expectation");
+        ASSERT_EQUAL_HINT(response[1].rating, (8 - 3)/2, "Rating does not match the expectation");
+        ASSERT_EQUAL_HINT(response[2].rating, (5 + 2 + 1 - 12)/4, "Rating does not match the expectation");
+    }
+}
+
+void TestDocumentSearchByStatus() {
     SearchServer server;
     server.SetStopWords("и в на"s);
     server.AddDocument(0, "белый кот и модный ошейник"s,        DocumentStatus::ACTUAL, {8, -3});
     server.AddDocument(1, "пушистый кот пушистый хвост"s,       DocumentStatus::REMOVED, {7, 2, 7});
     server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::IRRELEVANT, {5, -12, 2, 1});
     server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
-    //Если статус не верный
-    const auto& response_empty = server.FindTopDocuments("пушистый кот", DocumentStatus::IRRELEVANT);
-    ASSERT(response_empty.empty());
-    //При верном статусе
-    const auto& response = server.FindTopDocuments("пушистый кот", DocumentStatus::REMOVED);
-    ASSERT(!response.empty());
-    ASSERT_EQUAL(response[0].id, 1);
-    //Проверка работы передиката с рейтингом
-    const auto& peredicat_rating = server.FindTopDocuments("пушистый ухоженный кот", [](int document_id, DocumentStatus status, int rating){return rating == 9;});
-    ASSERT(!peredicat_rating.empty());
-    ASSERT_EQUAL(peredicat_rating.size(), 1);
-    ASSERT_EQUAL(peredicat_rating[0].rating, 9);
-    //ID
-    const auto& peredicat = server.FindTopDocuments("пушистый ухоженный кот", [](int document_id, DocumentStatus status, int rating){return document_id == 2;});
-    ASSERT(!peredicat.empty());
-    ASSERT_EQUAL(peredicat.size(), 1);
-    ASSERT_EQUAL(peredicat[0].id, 2);
+    {
+        const auto& response_empty = server.FindTopDocuments("пушистый кот", DocumentStatus::IRRELEVANT);
+        ASSERT(response_empty.empty());
+    }
+    {
+        const auto& response = server.FindTopDocuments("пушистый кот", DocumentStatus::REMOVED);
+        ASSERT_EQUAL(response.size(), 1);
+        ASSERT_EQUAL(response[0].id, 1);
+    }
+}
+
+void TestDocumentSearchByPredicat() {
+    SearchServer server;
+    server.SetStopWords("и в на"s);
+    server.AddDocument(0, "белый кот и модный ошейник"s,        DocumentStatus::ACTUAL, {8, -3});
+    server.AddDocument(1, "пушистый кот пушистый хвост"s,       DocumentStatus::REMOVED, {7, 2, 7});
+    server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::IRRELEVANT, {5, -12, 2, 1});
+    server.AddDocument(3, "ухоженный скворец евгений"s,         DocumentStatus::BANNED, {9});
+    {
+        const auto& predicat_rating = server.FindTopDocuments("пушистый ухоженный кот", [](int document_id, DocumentStatus status, int rating){return rating == 9;});
+        ASSERT_EQUAL(predicat_rating.size(), 1);
+        ASSERT_EQUAL(predicat_rating[0].id, 3);
+    }
+    {
+        const auto& predicat = server.FindTopDocuments("пушистый ухоженный кот", [](int document_id, DocumentStatus status, int rating){return document_id == 2;});
+        ASSERT_EQUAL(predicat.size(), 1);
+        ASSERT_EQUAL(predicat[0].id, 2);
+    }
+
 }
 
 void TestSearchServer() {
     RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
-    RUN_TEST(TestMinusPlusWords);
+    RUN_TEST(TestSearchesWithTheMinusWord);
     RUN_TEST(TestMatchDocuments);
-    RUN_TEST(TestRelevanseAndRating);
-    RUN_TEST(TestPeredicantAndStatus);
+    RUN_TEST(TestRelevanceSorting);
+    RUN_TEST(TestCorrectnessOfRelevanceCalculation);
+    RUN_TEST(TestCorrectnessOfRatingCalculation);
+    RUN_TEST(TestDocumentSearchByStatus);
+    RUN_TEST(TestDocumentSearchByPredicat);
 }
-
